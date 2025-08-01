6fedb4d068d13670b357a4506b1b26c74384ebfc
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2025-08-01 22:26:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,135 @@
+function copyMenu(){
+    var dptCategory = document.querySelector(".dpt-cat");
+    var dptPlace = document.querySelector(".departments");
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    var mainNav = document.querySelector(".header-nav nav");
+    var navPlace = document.querySelector(".off-canvas nav");
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    var topNav = document.querySelector(".header-top .wrapper");
+    var topPlace = document.querySelector(".off-canvas .thetop-nav");
+    topPlace.innerHTML = topNav.innerHTML;
+}
+
+copyMenu();
+
+const menuButton = document.querySelector(".trigger");
+const closeButton = document.querySelector(".t-close");
+const addclass = document.querySelector(".site");
+
+menuButton.addEventListener("click", function(){
+    addclass.classList.toggle("showmenu");
+});
+
+closeButton.addEventListener("click", function(){
+    addclass.classList.remove("showmenu");
+});
+
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e){
+    e.preventDefault();
+    submenu.forEach((item) => item != this 
+        ? item.closest(".has-child").classList.remove("expand") 
+        : null
+    );
+
+    if(this.closest(".has-child").classList != "expand"){
+        this.closest(".has-child").classList.add("expand");
+    }
+}
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination"
+    }
+});
+
+const searchButton = document.querySelector(".t-search");
+const tClose = document.querySelector(".search-close");
+const showClass = document.querySelector(".site");
+
+searchButton.addEventListener("click", function(){
+    showClass.classList.add("showsearch");
+});
+
+tClose.addEventListener("click", function(){
+    showClass.classList.remove("showsearch");
+});
+
+const dptButton = document.querySelector(".dpt-cat .dpt-trigger");
+const dptClass = document.querySelector(".site");
+
+dptButton.addEventListener("click", function(){
+    console.log("clicked");
+    dptClass.classList.toggle("showdpt");
+});
+
+var productThumb = new Swiper(".smapp-image", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        481: {
+            spaceBetween: 32,
+        }
+    }
+});
+
+var productBig = new Swiper(".big-image", {
+    loop: true,
+    autoHeight: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    thumbs: {
+        swiper: productThumb
+    }
+});
+
+var stocks = document.querySelectorAll(".products .stock");
+for(let x = 0; x < stocks.length; x++){
+    let stock = stocks[x].dataset.stock;
+    let available = stocks[x].querySelector(".qty-available").innerHTML;
+    let sold = stocks[x].querySelector(".qty-sold").innerHTML;
+    let percent = (sold / stock) * 100;
+
+    stocks[x].querySelector(".available").style.width = percent + "%";
+}
+
+
+const divToShow = ".mini-cart";
+const divPopup = document.querySelector(divToShow);
+const divTrigger = document.querySelector(".cart-trigger");
+
+
+
+divTrigger.addEventListener("click", () => {
+    setTimeout(() => {
+        if(!divPopup.classList.contains("show")){
+            divPopup.classList.add("show");
+        }
+    }, 250)
+});
+
+document.addEventListener("click", (e) => {
+    const isClosest = e.target.closest(divToShow);
+    if(!isClosest && divPopup.classList.contains("show")){
+        divPopup.classList.remove("show");
+    }
+});
+
+
+window.onload = function(){
+    document.querySelector(".site.page-checkout").classList.toggle("showmodal");
+}
+
+document.querySelector(".modalclose").addEventListener("click", () => {
+    document.querySelector(".site.page-checkout").classList.remove("showmodal");
+})
